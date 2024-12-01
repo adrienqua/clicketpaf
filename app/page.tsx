33b4025c8dbb8d3de246f6e9968d89ee6717975e
adrienqua <777adrien@gmail.com>
2024-12-01 05:43:50 +0100

    home content

--- app/page.tsx
+++ app/page.tsx
@@ -22,29 +22,40 @@ export const metadata: Metadata = {
 
 export default function Home() {
     return (
-        <main>
-            <h1>Click & Paf</h1>
-            <p>
-                Click & Paf a été fondée en 2022 par deux entrepreneurs passionnés de mode urbaine, Léo et Thomas. Leur
-                objectif était clair : offrir une sélection de vêtements tendance et de qualité, tout en restant ancrés
-                dans l’univers de la culture street. La boutique, située dans un quartier dynamique de la ville, se
-                distingue par son ambiance moderne et épurée, où chaque détail est pensé pour refléter l’identité de la
-                marque. Le nom "Click & Paf" symbolise à la fois la rapidité de la mode actuelle et l'impact immédiat
-                d'un look percutant.
-            </p>
-            <p>
-                Dès son ouverture, Click & Paf s’est rapidement imposée comme une référence grâce à une sélection
-                pointue de créateurs locaux et internationaux. Léo et Thomas ont misé sur des collaborations exclusives,
-                des pièces uniques, et des collections en édition limitée, attirant ainsi une clientèle jeune, exigeante
-                et en quête d'originalité. Leur approche se veut résolument moderne, avec un mélange de confort et
-                d’élégance qui plaît à ceux qui recherchent des pièces à la fois stylées et accessibles.
-            </p>
-            <p>
-                Aujourd'hui, Click & Paf continue de se développer, avec un site de vente en ligne qui permet de toucher
-                une clientèle plus large tout en restant fidèle à son ADN : une mode urbaine de qualité, innovante et
-                toujours à la pointe des tendances. La boutique est un lieu où la passion pour la mode se mêle à
-                l’ambition de proposer une expérience d'achat unique et personnalisée.
-            </p>
+        <main className="flex flex-col container mx-auto items-center justify-center min-h-screen max-w-[1200px]">
+            <h1 className="text-3xl font-bold mb-4">Click & Paf</h1>
+            <Image
+                src="/img/placeholder.png"
+                alt="Histoire de Clicketpaf"
+                width={300}
+                height={200}
+                className="rounded-lg mb-8"
+            />
+            <div>
+                <h2 className="text-2xl font-bold mb-4">Notre histoire</h2>
+                <p className="mb-4">
+                    Click & Paf a été fondée en 2022 par deux entrepreneurs passionnés de mode urbaine, Léo et Thomas.
+                    Leur objectif était clair : offrir une sélection de vêtements tendance et de qualité, tout en
+                    restant ancrés dans l’univers de la culture street. La boutique, située dans un quartier dynamique
+                    de la ville, se distingue par son ambiance moderne et épurée, où chaque détail est pensé pour
+                    refléter l’identité de la marque. Le nom "Click & Paf" symbolise à la fois la rapidité de la mode
+                    actuelle et l'impact immédiat d'un look percutant.
+                </p>
+                <p className="mb-4">
+                    Dès son ouverture, Click & Paf s’est rapidement imposée comme une référence grâce à une sélection
+                    pointue de créateurs locaux et internationaux. Léo et Thomas ont misé sur des collaborations
+                    exclusives, des pièces uniques, et des collections en édition limitée, attirant ainsi une clientèle
+                    jeune, exigeante et en quête d'originalité. Leur approche se veut résolument moderne, avec un
+                    mélange de confort et d’élégance qui plaît à ceux qui recherchent des pièces à la fois stylées et
+                    accessibles.
+                </p>
+                <p className="mb-4">
+                    Aujourd'hui, Click & Paf continue de se développer, avec un site de vente en ligne qui permet de
+                    toucher une clientèle plus large tout en restant fidèle à son ADN : une mode urbaine de qualité,
+                    innovante et toujours à la pointe des tendances. La boutique est un lieu où la passion pour la mode
+                    se mêle à l’ambition de proposer une expérience d'achat unique et personnalisée.
+                </p>
+            </div>
         </main>
     )
 }
