5dd8a863c777a71321448c9d1ff42c99340dfc12
Adrien Quacchia <777adrien@gmail.com>
2024-12-16 17:14:47 +0100

    fix: products

--- app/produits/[slug]/page.tsx
+++ app/produits/[slug]/page.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Click & Paf - Nos drops",
+  title: "Click & Paf - T-shirt François Bayrou",
   description:
-    "Découvrez nos vêtements en édition limitée, inspirés par l'actu et les tendances du moment. Style unique, engagé et audacieux pour une jeunesse qui ose se démarquer. Ne manquez pas nos créations exclusives, disponibles pour un temps limité !",
+    "Affiche ton soutien à François Bayrou avec ce t-shirt 'Premier ministre' ! Un design audacieux pour ceux qui suivent l'actualité de près et aiment porter leur opinion. Confortable, moderne et engagé, il est parfait pour ceux qui veulent marquer l’actualité politique avec style. Soyez au cœur du débat !",
   keywords: [
     "clicketpaf",
     "click & paf",
@@ -20,9 +20,17 @@ export const metadata: Metadata = {
 };
 
 export default function ProductDetails() {
+  const product = {
+    title: "T-shirt François Bayrou",
+    description:
+      "Affiche ton soutien à François Bayrou avec ce t-shirt 'Premier ministre' ! Un design audacieux pour ceux qui suivent l'actualité de près et aiment porter leur opinion. Confortable, moderne et engagé, il est parfait pour ceux qui veulent marquer l’actualité politique avec style. Soyez au cœur du débat !",
+    price: 24.9,
+  };
   return (
     <div className="flex flex-col items-center px-4">
-      <h1 className="text-3xl font-bold mb-10">Produit</h1>
+      <h1 className="text-3xl font-bold mb-10">{product.title}</h1>
+      <p className="font-semibold">{product.description}</p>
+      <p>{product.price.toFixed(2)}</p>
     </div>
   );
 }
