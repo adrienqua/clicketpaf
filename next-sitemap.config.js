21b2cb05e8f3331884d8e1a1ad630d33f0fd1ac6
adrienqua <777adrien@gmail.com>
2025-01-18 04:05:04 +0100

    dynamic sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,49 @@
+// Read the products data from the JSON file using fs
+import { productsDatas } from "./datas/products.js"
+import { articlesDatas } from "./datas/articles.js"
+// Export the products data
+
 /** @type {import('next-sitemap').IConfig} */
-module.exports = {
+const sitemap = {
     siteUrl: process.env.SITE_URL || "http://localhost:3000",
     generateRobotsTxt: true,
     priority: 1,
+
+    transform: async (config, path) => {
+        let changefreq = "monthly"
+        let priority = 0.7
+
+        if (path === "/") {
+            changefreq = "daily"
+            priority = 1
+        }
+
+        if (path.startsWith("/blog") || path.startsWith("/produits")) {
+            changefreq = "daily"
+            priority = 0.9
+        }
+
+        return {
+            loc: path,
+            changefreq,
+            priority,
+        }
+    },
+
+    additionalPaths: async (config) => {
+        const products = productsDatas.map((product) => ({
+            loc: `/produits/${product.slug}`,
+            changefreq: "daily",
+            priority: 0.9,
+        }))
+        const articles = articlesDatas.map((article) => ({
+            loc: `/blog/${article.category}/${article.slug}`,
+            changefreq: "daily",
+            priority: 0.9,
+        }))
+
+        return [...products, ...articles]
+    },
 }
+
+export default sitemap
