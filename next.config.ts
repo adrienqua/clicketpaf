db7578bd6ed44a39f619b69b6cb9f7d1764e14d8
adrienqua <777adrien@gmail.com>
2025-01-20 00:53:51 +0100

    gl

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,5 @@
 import type { NextConfig } from "next"
 
-const nextConfig: NextConfig = {
-    async headers() {
-        return [
-            {
-                source: "/:path*",
-                headers: [
-                    {
-                        key: "Content-Security-Policy",
-                        value: "default-src 'self'; script-src 'self' 'nonce-googleAnalytics' 'unsafe-inline'",
-                    },
-                ],
-            },
-        ]
-    },
-}
+const nextConfig: NextConfig = {}
 
 export default nextConfig
